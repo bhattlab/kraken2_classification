2568160b1cb17dbba63a421d57e698895ef6143b
Ben Siranosian <bsiranosian@gmail.com>
2019-01-24 08:11:49 -0800

    small script fix

--- scripts/extract_taxid_reads.sh
+++ scripts/extract_taxid_reads.sh
@@ -4,7 +4,7 @@ if [ $#!=4 ]; then
     echo "USAGE: `basename $0` input.krak taxid reads_1.fq reads_2.fq out_1.fq out_2.fq"
     echo "Currently only gets reads directly assigned to taxid, not further classified"
     echo "To get reads for a genus, run for the genus and all species underneath"
-    echo "Stores read identifiers in kraken_$taxid_reads.txt in the current working directory"
+    echo "Stores read identifiers in kraken_taxid_reads.txt in the current working directory"
     echo "Requires filterbyname.sh from the bbmap package to work"
     exit 85
 fi
@@ -20,4 +20,3 @@ awk -v taxid="$taxid" '$3==taxid { print }' "$in_krak" | cut -f 2 > kraken_"$tax
 
 filterbyname.sh in="$in_1" in2="$in_2" names=kraken_"$taxid"_reads.txt include=true out="$out_1" out2="$out_2"
 
-    
