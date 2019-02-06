4cb521a87b8da6bbe9f15e92fec5b7730e07ba35
Ben Siranosian <bsiranosian@gmail.com>
2019-02-06 09:12:09 -0800

    ,

--- scripts/extract_taxid_reads.sh
+++ scripts/extract_taxid_reads.sh
@@ -1,6 +1,7 @@
-#!/usr/bin/env/ bash
+#!/usr/bin/env bash
 
-if [ $#!=4 ]; then 
+if [ $# -ne 6 ]; then 
+    echo "$#"
     echo "USAGE: `basename $0` input.krak taxid reads_1.fq reads_2.fq out_1.fq out_2.fq"
     echo "Currently only gets reads directly assigned to taxid, not further classified"
     echo "To get reads for a genus, run for the genus and all species underneath"
@@ -17,6 +18,12 @@ out_1=$5
 out_2=$6
 
 awk -v taxid="$taxid" '$3==taxid { print }' "$in_krak" | cut -f 2 > kraken_"$taxid"_reads.txt
-
-filterbyname.sh in="$in_1" in2="$in_2" names=kraken_"$taxid"_reads.txt include=true out="$out_1" out2="$out_2"
+# only do this if there are some lines in the file, elese exit with error
+if [[ $(wc -l < kraken_"$taxid"_reads.txt) -ge 0 ]]
+then
+    filterbyname.sh in="$in_1" in2="$in_2" names=kraken_"$taxid"_reads.txt include=true out="$out_1" out2="$out_2"
+else
+    echo "No reads found with $taxid"
+    exit 1
+fi 
 
